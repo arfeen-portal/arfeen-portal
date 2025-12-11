236f3814b44e96bc6fb8220819971ba3b4424aae
Syed Hassan Jawad <arfeentravel@gmail.com>
2025-12-11 17:45:06 +0500

    Fix leader page module export

---
+++ src/app/(dashboard)/groups/[groupId]/leader/page.tsx
@@ -0,0 +1,8 @@
+export default function LeaderPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-semibold">Group Leader Dashboard</h1>
+      <p>Leader page is working.</p>
+    </div>
+  );
+}
